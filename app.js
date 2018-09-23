f560e802325a2d9aafefcd80ae403ac2b94c28f1
Aashish Chauhan <ashishc534@gmail.com>
2018-09-23 22:50:27 +0530

    Setted up Express, Ejs view engine and body-parser.

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+var urlEncodedParser = bodyParser.urlencoded({extended:false});
+
+//Set up view engine.
+app.set("view engine","ejs");
+
+//Static files
+app.use(express.static(./public));
+
+//Fire Up Controllers
+todoController(app);
+
+//Listen to port
+app.listen(3000);
+console.log("Listening to port:3000.");
