0723a7ca59aa3051c641f66d8cc74b0c254377bb
Aashish Chauhan <ashishc534@gmail.com>
2018-09-24 23:43:27 +0530

    Added Complete, Un-complete and Remove functionality

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,28 +1,84 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var urlEncodedParser = bodyParser.urlencoded({extended:false});
 
 var data = [{item:"Add Milk"},{item:"Make List"},{item:"Clean Garage"},{item:"Practice Node.JS"}];
 
+//Connect to database
+mongoose.connect('mongodb://ashish:abcd1234@ds255767.mlab.com:55767/todos');
+
+//Create schema - Like a blueprint
+var todoSchema = new mongoose.Schema({
+  item: String,
+  completed: Boolean
+});
+
+//Create Model (Model name starts with captital letter)
+var Todo = mongoose.model('Todo',todoSchema);
+
 module.exports = function(app){
 
-    app.get('/todo',function(req,res){
+    app.get(['/todo','/'],function(req,res){
 
-      res.render('todo',{todos:data});
+      var ongoing = [];
+      var completed = [];
+
+      Todo.find({completed:false},function(err,data){
+        if(err)
+        throw err;
+
+        ongoing = data;
+
+          Todo.find({completed:true},function(err,data){
+            if(err)
+            throw err;
+
+          completed = data;
+
+          res.render('todo',{completed:completed,ongoing:ongoing});
+        });
+      });
     });
 
     app.post('/todo', urlEncodedParser, function(req,res){
 
-      data.push(req.body);
-      res.json(data);
+      var newTodo = Todo(req.body).save(function(err,data){
+        if(err)
+        throw err;
+
+        res.json(data);
+      });
+    });
+
+    app.post('/todo/complete/:item',urlEncodedParser,function(req,res){
+
+        var completedTodo = Todo.findOneAndUpdate({item:req.params.item.replace(/\-/g," "),},{$set:{completed:true}},function(err,data){
+          if(err)
+          throw err;
+
+          res.json(data);
+        });
+    });
+
+    app.post('/todo/uncomplete/:item', urlEncodedParser,function(req,res){
+
+      var unCompletedTodo = Todo.findOneAndUpdate({item:req.params.item.replace(/\-/g," "),},{$set:{completed:false}},function(err,data){
+
+        if(err)
+        throw err;
+
+        res.json(data);
+      });
     });
 
     app.delete('/todo/:item',function(req,res){
 
-      //Removing Item.
-      data = data.filter(function(todo){
-        return todo.item.replace(/ /g, "-") !== req.params.item;
-      });
+      //Replace "-" with " " (as original before replacing with -) and then remoeve it.
+      Todo.find({item:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+        if(err)
+        throw err;
 
-      res.json(data);
+        res.json(data);
+      });
     });
 };
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $('#submit_item').on('click',function() {
 
     var item = $('#add_item');
-    var todo = {item: item.val()};
+    var todo = {item: item.val(),completed:false};
 
     $.ajax({
 
@@ -17,9 +17,9 @@ $(document).ready(function(){
     });
   });
 
-  $('li').on('click',function(){
+  $('#remove').on('click',function(){
 
-    var item = $(this).text().replace(/ /g, "-");
+    var item = event.target.parentNode.getAttribute('id').replace(/ /g,"-");
 
     $.ajax({
 
@@ -27,6 +27,33 @@ $(document).ready(function(){
       url:'/todo/' + item,
       success: function(data){
 
+        location.reload();
+      }
+    });
+  });
+
+  $('#completed').on('click',function(){
+
+    var item = event.target.parentNode.getAttribute('id').replace(/ /g,"-");
+    $.ajax({
+
+      type: 'POST',
+      url: '/todo/complete/' + item,
+      success: function(data){
+
+        location.reload();
+      }
+    });
+  });
+
+  $('#un_completed').on('click',function(){
+
+    var item = event.target.parentNode.getAttribute('id').replace(/ /g,"-");
+    $.ajax({
+
+      type: 'POST',
+      url: '/todo/uncomplete/' + item,
+      success: function(data){
         location.reload();
       }
     });
